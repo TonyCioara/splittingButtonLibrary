df186af14a73e2f7bfabf0e54dd10d733e152fae
TonyCioara <tonyangelo9707@gmail.com>
2018-05-10 14:45:27 -0700

    first commit

---
+++ splittingButtonLibrary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  splittingButtonLibrary
+//
+//  Created by Tony Cioara on 5/10/18.
+//  Copyright © 2018 Tony Cioara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
